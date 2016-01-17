7513c721792e33767a60dd2c6da9a6e9bba1bf43
Hieu Tran <tr.kimhieu@gmail.com>
2016-01-18 00:43:42 +0700

    Change setup and assert methods

--- app/src/test/java/me/kimhieu/snowballvideoplayer/AppDataTest.java
+++ app/src/test/java/me/kimhieu/snowballvideoplayer/AppDataTest.java
@@ -1,20 +1,38 @@
 package me.kimhieu.snowballvideoplayer;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 /**
  * Created by kimhieu on 1/18/16.
  */
 public class AppDataTest {
 
+    AppData appData;
+    List<String> testList;
+
+
+    @Before
+    public void setUp() throws Exception {
+        appData = AppData.getInstance();
+        testList = appData.videoList;
+    }
+
     @Test
     public void makeSureObjectHas3Items() throws Exception {
-        AppData appData = AppData.getInstance();
-        List<String> testList = appData.videoList;
         assertEquals(testList.size(),3);
+    }
+
+    @Test
+    public void makeSureVideoListIsArrayList() throws Exception {
+        assertThat(testList, instanceOf(ArrayList.class));
     }
 }
