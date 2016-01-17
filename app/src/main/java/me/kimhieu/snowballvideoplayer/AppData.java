02f42f2ebc2c08f7a44f5e53ae713a2178ab2acb
Hieu Tran <tr.kimhieu@gmail.com>
2016-01-18 00:31:06 +0700

    Create AppData to handle hardcode data

--- app/src/main/java/me/kimhieu/snowballvideoplayer/AppData.java
+++ app/src/main/java/me/kimhieu/snowballvideoplayer/AppData.java
@@ -1,7 +1,26 @@
 package me.kimhieu.snowballvideoplayer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by kimhieu on 1/18/16.
  */
 public class AppData {
+
+    public static AppData instance;
+    public static List<String> videoList = new ArrayList<>();
+
+    public AppData(){
+        videoList.add("http://media.snowballnow.com/video/upload/v1451206914/lcgbutfkqbkhvrfdurrp.mp4");
+        videoList.add("http://media.snowballnow.com/video/upload/v1450752299/deff3bxsmpsrow4i40e0.mp4");
+        videoList.add("http://media.snowballnow.com/video/upload/v1450787135/poareeehaxfqhcyavnnl.mp4");
+    }
+
+    public static AppData getInstance(){
+        if(instance == null) {
+            instance = new AppData();
+        }
+        return instance;
+    }
 }
